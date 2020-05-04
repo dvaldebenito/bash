81ec30e06fff80a5c02f369b20778d855ae8b57c
Daniel Valdebenito González <danielvaldebenitogonzalez@gmail.com>
2020-05-03 21:46:25 -0400

    Se actualiza script

--- Renombrar archivos/masivo.sh
+++ Renombrar archivos/masivo.sh
@@ -6,16 +6,16 @@ contador=0
 temp=0
 
 if [ $# -lt 2 ]; then
-  echo El script recibi√≥ menos de 2 par√°metros, ejemplo: ./sc.sh .mp4 S01E
+  echo El script recibi®Æ menos de 2 par®¢metros, ejemplo: ./sc.sh .mp4 S01E
   exit 2
 fi
 
 if [ $# -gt 0 ] && [ -z $1 ]; then
-  echo El script recibi√≥ par√°metros, ejemplo: ./sc.sh .mp4 S01E
+  echo El script recibi®Æ par®¢metros, ejemplo: ./sc.sh .mp4 S01E
   exit 3
 fi
 
-for i in $( ls *$criterio ); 
+for i in *$criterio;
 do
  contador=`expr $contador + 1`
  if [ $contador -lt 10 ]; then
@@ -25,7 +25,7 @@ do
  fi 
  #orig=$i
  #dest=$(echo $i | sed -e "s/$expresion/$sustituto/")
- mv $i $2$temp$1
+ mv "$PWD/$i" "$PWD/$2$temp$1"
  #echo $i
  #echo $2$temp$1
 done
